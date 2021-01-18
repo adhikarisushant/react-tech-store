efae903c1f16653375f202016745177f5707dc9d
Sushant <sushant.adhikari.619@gmail.com>
2021-01-18 10:00:14 +0545

    product context setup

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,14 @@
-// products context
+import React from 'react';
+
+export const ProductContext = React.createContext();
+
+
+export default function ProductProvider({children}) {
+    const greeting = "hello";
+    const product = { id: 1, title: "product name" };
+    return (
+        <ProductContext.Provider value={{ greeting, product }}>
+            {children}
+        </ProductContext.Provider>
+    );
+}
